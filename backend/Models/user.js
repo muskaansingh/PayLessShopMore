bdefbdcdf850fc8f527eee3a5b20225c9cfdc706
Muskaan <muskaansingh235@gmail.com>
2021-04-10 15:14:05 +0530

    added virtual fields and encrypted password in user schema

--- backend/Models/user.js
+++ backend/Models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto');
+const uuid = require('uuid/v1');
 
 const userSchema = new Schema({
     name: {
@@ -23,9 +25,9 @@ const userSchema = new Schema({
         type: String,
         trim: true
     },
-    password:{
+    encrypt_password:{
         type: String,
-        trim: true
+        required: true
     },
     salt: String,
     // role is basically either user, admin, tech team, etc.
@@ -38,5 +40,37 @@ const userSchema = new Schema({
         default: []
     }
 })
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password
+        this.salt = uuidv1();
+        this.encrypt_password = this.securePassword(password);
+    })
+    .get(function(){
+        return this._password;
+    })
+
+userSchema.method = {
+
+    authenticate: function(plainPassword){
+        return {
+            this.securePassword(plainPassword) === this.encrypt_password
+        }
+    },
+
+    securePassword: function(plainPassword){
+        if(!password) {
+            return ""
+        }
+        try {
+            return crypto.createHmac('sha256', this.salt)
+            .update(plainPassword)
+            .digest('hex')            
+        } catch (err) {
+            return ""
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema)
